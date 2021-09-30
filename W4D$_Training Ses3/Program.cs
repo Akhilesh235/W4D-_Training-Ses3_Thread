0f4bf79ce64f574bf1da4d2ad7f704a036266782
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-30 17:44:03 +0800

    Asynchornous Order of Execution

--- W4D$_Training Ses3/Program.cs
+++ W4D$_Training Ses3/Program.cs
@@ -14,18 +14,30 @@ namespace W4D__Training_Ses3
             Console.WriteLine("In CallDisplayAsynchronously ");
             Thread t = new Thread(() => { Display(); }); //line 15 is the same as line 17 and 18 combined
 
-            ThreadStart start = new ThreadStart(Display);            
+            ThreadStart start = new ThreadStart(Display1);            
             Thread t1 = new Thread(start);
-            Console.WriteLine("Before Starting Thread");
+            Console.WriteLine("Before Starting Thread"); 
            
+            t.Start(); // will only be called after line 23
+            t1.Start(); //will only be called after line 21
+            Console.WriteLine("Started Thread"); // will be called after line 19
+            t = new Thread(() => { Display2(); });
             t.Start();
-            t1.Start();
-            Console.WriteLine("Started Thread");
         }
 
-        public void Display()
+        public void Display()               
         {
-            Console.WriteLine("I am in display");
+            Console.WriteLine("I am in display");       //the order of execution will be different
+        }
+
+        public void Display1()
+        {
+            Console.WriteLine("I am in display1");
+        }
+
+        public void Display2()
+        {
+            Console.WriteLine("I am in display2");
         }
     }
 
