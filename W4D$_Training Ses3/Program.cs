a60b55dac379253df7b39e5328ef71186b3c78b3
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-30 18:05:08 +0800

    Asynchoronous Parameter Thread

--- W4D$_Training Ses3/Program.cs
+++ W4D$_Training Ses3/Program.cs
@@ -13,13 +13,16 @@ namespace W4D__Training_Ses3
         {
             Console.WriteLine("In CallDisplayAsynchronously ");
             Thread t = new Thread(() => { Display(); }); //line 15 is the same as line 17 and 18 combined
+            Thread tp = new Thread(new ParameterizedThreadStart(DisplayParameter));
 
             ThreadStart start = new ThreadStart(Display1);            
             Thread t1 = new Thread(start);
-            Console.WriteLine("Before Starting Thread"); 
-           
-            t.Start(); // will only be called after line 23
-           // t1.Start(); //will only be called after line 21
+            //ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart();
+            Console.WriteLine("Before Starting Thread");
+
+            //t.Start(); // will only be called after line 23
+            // t1.Start(); //will only be called after line 21
+            tp.Start("this is now");
             Console.WriteLine("Started Thread"); // will be called after line 19
                                                  // t = new Thread(() => { Display2(); });
                                                  // t.Start();
@@ -29,6 +32,15 @@ namespace W4D__Training_Ses3
                 if (i == 2)
                     t.Join();
                 Console.WriteLine("I am in main thread");       //the order of execution will be different
+            }
+        }
+
+        public void DisplayParameter(object str)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("I am in display");       //the order of execution will be different
             }
         }
 
